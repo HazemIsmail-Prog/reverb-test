27c44c9e6270b5bca2ad99a09a2c0ab49ac8fb1b
Hazem <hazem.ismail@hotmail.com>
2024-09-21 20:18:08 +0300

    build

--- breeze-front/src/lib/axios.js
+++ breeze-front/src/lib/axios.js
@@ -1,22 +1,17 @@
 import Axios from 'axios'
 
-// Function to retrieve a cookie by its name
-const getCookie = id => {
-
-    let value = document.cookie.match('(^|;)?' + id + '=([^;]*)(;|$)');
-    return value ? unescape(value[2]) : null;
- 
- }
-
-// Create an Axios instance with default configuration
 const axios = Axios.create({
-    baseURL: import.meta.env.VITE_PUBLIC_BACKEND_URL, // Backend API URL from environment variables
+    baseURL: import.meta.env.VITE_PUBLIC_BACKEND_URL,
     headers: {
-        'Content-Type': 'application/json', // Specify JSON content type
-        Accept: 'application/json', // Accept JSON responses
-        'X-XSRF-TOKEN': getCookie('XSRF-TOKEN'), // Include the CSRF token from the cookie
+        
     },
-    withCredentials: true, // Send cookies with requests (including CSRF token)
+    headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    withCredentials: true,
+    withXSRFToken: true,
 })
 
 export { axios }
