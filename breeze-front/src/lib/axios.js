18af7b3963b7f1d0fc62ae954adf0148734752e1
Hazem <hazem.ismail@hotmail.com>
2024-09-21 19:01:56 +0300

    build

--- breeze-front/src/lib/axios.js
+++ breeze-front/src/lib/axios.js
@@ -3,7 +3,7 @@ import Axios from 'axios'
 const axios = Axios.create({
     baseURL: import.meta.env.VITE_PUBLIC_BACKEND_URL,
     headers: {
-        'X-Requested-With': 'XMLHttpRequest',
+        // 'X-Requested-With': 'XMLHttpRequest',
         "Content-Type": "application/json",
     },
     withCredentials: true,
