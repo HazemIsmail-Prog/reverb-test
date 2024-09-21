a7d5912e187ed6c8b4cce48784646093d5b6405e
Hazem <hazem.ismail@hotmail.com>
2024-09-21 19:48:00 +0300

    build

--- breeze-front/src/lib/axios.js
+++ breeze-front/src/lib/axios.js
@@ -4,11 +4,17 @@ const axios = Axios.create({
     baseURL: import.meta.env.VITE_PUBLIC_BACKEND_URL,
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
-        "Content-Type": "application/json",
-        "Accept": "application/json",
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        'X-XSRF-TOKEN': getCookie('XSRF-TOKEN'),
     },
-    withCredentials: true,
-    withXSRFToken: true,
+    withCredentials: true, // Automatically send cookies, including the CSRF token
 })
+
+function getCookie(name) {
+    const value = `; ${document.cookie}`
+    const parts = value.split(`; ${name}=`)
+    if (parts.length === 2) return parts.pop().split(';').shift()
+}
 
 export { axios }
